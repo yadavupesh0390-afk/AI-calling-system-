2d616c69a7a9ee5dc10aace7e043dca4e54fa4f5
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-05-15 11:49:18 +0530

    Create leadStatus.js

---
+++ shared/constants/leadStatus.js
@@ -0,0 +1,8 @@
+module.exports = {
+  INTERESTED: 'Interested',
+  CALLBACK_LATER: 'Callback Later',
+  NOT_INTERESTED: 'Not Interested',
+  NO_ANSWER: 'No Answer',
+  BUSY: 'Busy',
+  INVALID: 'Invalid Number'
+};

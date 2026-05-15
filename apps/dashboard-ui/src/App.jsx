a11d283ea6481f67501007b09eb2b4797bc80f02
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-05-15 21:19:09 +0530

    Update App.jsx

--- apps/dashboard-ui/src/App.jsx
+++ apps/dashboard-ui/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

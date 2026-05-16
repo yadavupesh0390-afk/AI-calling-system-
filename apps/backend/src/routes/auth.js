296376ec250c45578c8646d6314b0b7854f52ced
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-05-16 21:28:29 +0530

    Update auth.js

--- apps/backend/src/routes/auth.js
+++ apps/backend/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-//const { authenticateToken } = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
 

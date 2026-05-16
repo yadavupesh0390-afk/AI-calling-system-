414c1a4b3bda9f5d8a621a8f9d8ee68c4dd98be9
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-05-16 12:55:43 +0530

    Update main.jsx

--- apps/dashboard-ui/src/main.jsx
+++ apps/dashboard-ui/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>Hello World</h1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );

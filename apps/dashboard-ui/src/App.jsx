1cd0c759078b16956860e2df08b607b7f6b86e04
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-05-16 22:37:02 +0530

    Update App.jsx

--- apps/dashboard-ui/src/App.jsx
+++ apps/dashboard-ui/src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import CreateCampaign from './pages/CreateCampaign';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+
+        {/* ✅ ADD THIS INSIDE ROUTES */}
+        <Route path="/create-campaign" element={<CreateCampaign />} />
+
       </Routes>
     </BrowserRouter>
   );
